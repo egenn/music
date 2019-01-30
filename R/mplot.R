a7493695627f6178aafb7e6159a3258ca6b48849
Stathis Gennatas <gennatas@gmail.com>
2019-01-29 21:46:35 -0800

    remove y

--- R/mplot.R
+++ R/mplot.R
@@ -5,7 +5,6 @@
 #' Plot waveform
 #'
 #' @param x Input
-#' @param y Optional input
 #' @param type String: "l" for lines, "p" for points. Default = "l"
 #' @param main String: Plot title
 #' @param pty String: "m" to fill available device space, "s" for square plot. Default = "m"
@@ -24,7 +23,7 @@
 #' @export
 #' @author Efstathios D. Gennatas
 
-mplot <- function(x, y = NULL,
+mplot <- function(x,
                   type = "l",
                   main = NULL,
                   pty = "m",
@@ -47,9 +46,10 @@ mplot <- function(x, y = NULL,
   if (is.null(mar)) mar <- if (.ncol == 1) c(1.2, 1.2, .4, .4) else c(0, 0, 0, 0)
   if (is.null(oma)) oma <- if (.ncol == 1) rep(0, 4) else c(1.2, 1.2, 1.2, 1.2)
   par(bg = bg, mar = mar, mfrow = c(.ncol, 1), oma = oma)
+  .names <- colnames(x)
 
   if (.ncol == 1) {
-    plot(x, y = y,
+    plot(x, y = NULL,
          type = type,
          xlab = "",
          ylab = "Amplitude",
@@ -63,9 +63,10 @@ mplot <- function(x, y = NULL,
          yaxt = yaxt,
          mgp = mgp)
     if (!is.null(main)) mtext(main, col = col.main, line = 1, adj = 0, font = 2, xpd = TRUE)
+    if (!is.null(.names)) mtext(.names, col = col.main, line = -1, adj = .99)
   } else {
     for (i in seq(.ncol)) {
-      plot(x[, i], y = y,
+      plot(x[, i], y = NULL,
            type = type,
            xlab = "",
            ylab = "Amplitude",
@@ -79,6 +80,7 @@ mplot <- function(x, y = NULL,
            yaxt = yaxt,
            mgp = mgp)
       if (i == 1) if (!is.null(main)) mtext(main, col = col.main, line = 1, adj = 0, font = 2, xpd = TRUE)
+      if (!is.null(.names)) mtext(.names[i], col = col.main, line = -1.5, adj = .99)
     }
   }
 
