fe5178b929c9a4470632632e1d5cd330c431face
Stathis Gennatas <gennatas@gmail.com>
2019-01-30 18:55:49 -0800

    round n.samples

--- R/freq2wave.R
+++ R/freq2wave.R
@@ -20,35 +20,39 @@ freq2wave <- function(frequency,
                       plot = FALSE) {
 
   oscillator <- match.arg(oscillator)
-  t <- seq(0, duration / BPM * 60, 1 / sample.rate)
+  .t <- seq(0, duration / BPM * 60, 1 / sample.rate)
   n.freqs <- length(frequency)
 
+  # Wave ====
   wave <- sapply(seq(n.freqs), function(i)
     switch(oscillator,
-           sine = sin(t * frequency[i] * 2 * pi),
-           square = sign(sin(t * frequency[i] * 2 * pi)),
-           saw = 2 * (t * frequency[i] - floor(.5 + t * frequency[i])),
-           triangle = 2 * abs(2 * (t * frequency[i] - floor(.5 + t * frequency[i]))) - 1))
+           sine = sin(.t * frequency[i] * 2 * pi),
+           square = sign(sin(.t * frequency[i] * 2 * pi)),
+           saw = 2 * (.t * frequency[i] - floor(.5 + .t * frequency[i])),
+           triangle = 2 * abs(2 * (.t * frequency[i] - floor(.5 + .t * frequency[i]))) - 1))
 
   n.samples <- NROW(wave)
 
+  # Attack ====
   if (attack.time > 0) {
     attack <- rep(1, n.samples)
     # n.attack.samples <- attack.time / 1000 * sample.rate
     # If attack.time was inappropriately long for your waveform, you may have longer attack than wave
-    n.attack.samples <- min(n.samples, attack.time / 1000 * sample.rate)
+    n.attack.samples <- round(min(n.samples, attack.time / 1000 * sample.rate))
     attack[seq(n.attack.samples)] <- seq(0, 1, length.out = n.attack.samples)
     wave <- wave * attack
   }
 
+  # Inner Release ====
   if (inner.release.time > 0) {
     inner.release <- rep(1, n.samples)
-    n.inner.release.samples <- min(n.samples, inner.release.time / 1000 * sample.rate)
+    n.inner.release.samples <- round(min(n.samples, inner.release.time / 1000 * sample.rate))
     inner.release[n.samples - (seq(n.inner.release.samples) - 1)] <- seq(0, 1, length.out = n.inner.release.samples)
     wave <- wave * inner.release
   }
 
-  colnames(wave) <- names(frequency)
+  .names <- if (!is.null(names(frequency))) names(frequency) else paste(frequency, "Hz")
+  colnames(wave) <- .names
   if (plot) mplot(wave)
   wave
 
