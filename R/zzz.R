ab61304e7472213111b6755a78429dad981b071c
Stathis <gennatas@gmail.com>
2022-10-16 15:23:05 -0700

    cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -4,12 +4,12 @@
 
 #' \pkg{music}: Learn and use music theory
 #'
-#' The music package allows you to build, play, and visualize scales, chords, 
+#' The music package allows you to build, play, and visualize scales, chords,
 #' and chord progression.
-#' For playback, \pkg{music} builds waveforms as matrices and passes them to 
+#' For playback, \pkg{music} builds waveforms as matrices and passes them to
 #' the \pkg{audio} package which interfaces with the system's audio driver.
-#' The default notation and frequencies used throughout the package are based 
-#' on twelve-tone equal temperament tuning (12ET). Custom tuning can be defined 
+#' The default notation and frequencies used throughout the package are based
+#' on twelve-tone equal temperament tuning (12ET). Custom tuning can be defined
 #' by specifying frequency ratios and a root  note. See \link{note2freq}.
 #' A4 defaults to 440Hz, and can be changed with the 'A4' argument.
 #' @docType package
@@ -19,10 +19,12 @@
 NULL
 
 .onAttach <- function(libname, pkgname) {
-  music.ver <- read.dcf(file = system.file("DESCRIPTION", package = pkgname),
-                         fields = "Version")
-  packageStartupMessage(paste0("  .:", pkgname, " v", music.ver,
-                               " \U1F3A7 Welcome, ", Sys.getenv("USER"),
-                               "\n  [", sessionInfo()[2], "]"))
-
+    music.ver <- read.dcf(
+        file = system.file("DESCRIPTION", package = pkgname),
+        fields = "Version"
+    )
+    packageStartupMessage(paste0(
+        "  .:", pkgname, " v", music.ver,
+        " \U1F3A7 Welcome, ", Sys.getenv("USER")
+    ))
 }
